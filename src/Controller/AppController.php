42c5e3e8d38985b143c0f0e32345bbbff0e06e1a
Thomas75019 <tlarousse3@gmail.com>
2019-01-29 02:21:07 +0100

    Entity and updates of the controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,14 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Annotation;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
 
 class AppController extends AbstractController
 {
     /**
      * @Route("/suscribe", name="suscribe")
      */
-    public function suscribe()
+    public function suscribe(User $user)
     {
+        $em = $this->getDoctrine()->getManager();
+        $user = new User();
+        $user->setIsActivated(false);
+
         return new Response('Suscribe page');
     }
 
